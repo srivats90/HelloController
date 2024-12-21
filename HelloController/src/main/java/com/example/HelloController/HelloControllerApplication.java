6b157b90d6f539d74689e71e781c4077f6af8ba4
Srivatsan <srivatsan90@yahoo.in>
2024-12-20 21:21:30 -0500

    Hello Controller sprinngboot project

---
+++ HelloController/src/main/java/com/example/HelloController/HelloControllerApplication.java
@@ -0,0 +1,12 @@
+package com.example.HelloController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloControllerApplication.class, args);
+	}
+}
